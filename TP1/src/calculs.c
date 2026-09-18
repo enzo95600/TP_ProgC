c69a5af53d1ac75bf4174980fb56c4c009e4cc81
enzo95600 <sentenzo951@gmail.com>
2026-09-18 09:11:10 +0000

    Exercice1.8

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main(void) {
+    int num1 = 12;
+    int num2 = 5;
+    char op = '&';
+
+    switch (op) {
+        case '+': printf("%d\n", num1 + num2); break;
+        case '-': printf("%d\n", num1 - num2); break;
+        case '*': printf("%d\n", num1 * num2); break;
+        case '/': printf("%d\n", num1 / num2); break;
+        case '%': printf("%d\n", num1 % num2); break;
+        case '&': printf("%d\n", num1 & num2); break;
+        case '|': printf("%d\n", num1 | num2); break;
+        case '~': printf("%d\n", ~num1); break; /* Opérateur unaire : applique seulement sur num1 */
+        default: printf("Opérateur invalide\n"); break;
+    }
+
+    return 0;
+}
