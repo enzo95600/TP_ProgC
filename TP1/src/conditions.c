c53c761e46ae135d7cb82b1eac9a4b72e9dfc7c7
enzo95600 <sentenzo951@gmail.com>
2026-09-18 09:08:13 +0000

    Exercice1.7

---
+++ TP1/src/conditions.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+    int somme = 0;
+
+    for (int i = 1; i <= 1000; i++) {
+        /* Si divisible par 11, on passe directement au nombre suivant */
+        if (i % 11 == 0) {
+            continue;
+        }
+
+        /* Si divisible par 5 ou 7, on l'ajoute a la somme */
+        if (i % 5 == 0 || i % 7 == 0) {
+            somme += i;
+        }
+
+        /* Si la somme dépasse 5000, on arrête immédiatement la boucle */
+        if (somme > 5000) {
+            break;
+        }
+    }
+
+    printf("Somme finale : %d\n", somme);
+
+    return 0;
+}
