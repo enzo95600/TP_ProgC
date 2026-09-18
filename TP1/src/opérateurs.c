98339ba4c2e829985a4c1b53fca2b1f14dfcef8c
enzo95600 <sentenzo951@gmail.com>
2026-09-18 08:55:41 +0000

    Exercice1.5

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,2 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
 
+int main(void) {
+    int a = 16;
+    int b = 3;
 
+    /* Opérateurs arithmétiques */
+    printf("a + b = %d\n", a + b);
+    printf("a - b = %d\n", a - b);
+    printf("a * b = %d\n", a * b);
+    printf("a / b = %d\n", a / b);
+    printf("a %% b = %d\n", a % b);
+
+    /* Opérateurs de comparaison (booléens) */
+    printf("a == b : %s\n", (a == b) ? "true" : "false");
+    printf("a > b  : %s\n", (a > b) ? "true" : "false");
+
+    return 0;
+}
